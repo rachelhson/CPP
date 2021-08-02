2992d1ad21d9e6428e1daed8bed6159f4b792b0f
Rachel Hyo Son <52179017+rachelhson@users.noreply.github.com>
2021-08-02 15:08:32 -0400

    Create array_type.cpp

---
+++ data_type/array/array_type.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+
+
+int main() {
+
+    int guesses[] = {10, 13, 54, 42, 12, 12, 13};
+    int size = sizeof(guesses) / sizeof(guesses[0]); // sizeof - bit size
+    std::cout <<sizeof(guesses[0]) <<std::endl;   
+    std::cout <<size <<std::endl; 
+
+    for (int i = 0; i <size; i++){
+        std::cout<<guesses[i]<<std::endl;
+    }
+
+
+    std::cout << guesses[3] <<std::endl;
+    guesses[3] = 300;
+    std::cout << guesses[3] <<std::endl;
+    return 0;
+}
