f097e988b6e8ae6e7b39c2dc4355cbca1a576336
Rachel Hyo Son <52179017+rachelhson@users.noreply.github.com>
2021-08-13 10:04:10 -0400

    Update math_function.cpp

--- numeric_function/math_function.cpp
+++ numeric_function/math_function.cpp
@@ -12,7 +12,10 @@ int main(){
     std::cout << ceil (fmin(10,3.25)) << std::endl; // go up
     std::cout << floor (fmin(10,3.25)) << std::endl; // go down
     std::cout << trunc (-1.25) << std::endl; // crop
+    std::cout << floor(fmin(10, -3.25)) <<std::endl; // vs. trunc different for neg. values
+    std::cout << trunc(fmin(10, -3.25)) <<std::endl;
     std::cout << round(-1.49) << std::endl; // whatever closest `nearby`
+    std::cout << nearbyint(3.25) <<std::endl;
 
     return 0;
 }
