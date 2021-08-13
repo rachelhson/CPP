d8abcce697bf6e2148f00783cbf55a413f25061c
Rachel Hyo Son <52179017+rachelhson@users.noreply.github.com>
2021-08-13 10:14:27 -0400

    Add files via upload

--- data_type/stringclass/stringclass.cpp
+++ data_type/stringclass/stringclass.cpp
@@ -5,8 +5,11 @@ int main(){
     
     std::string greeting = "hello";
     std::cout << greeting + "hhhh" <<std::endl;
-    std::cout << greeting.length() <<std::endl;
+    std::cout << greeting.length() <<std::endl; // number of characters
+    
+    greeting += "!"; // append
 
+    std::cout << greeting <<std::endl;
     //method == member function == functions attached to objects
     char name[] = "Rachel"; //c string == array of characters 
 
@@ -14,6 +17,6 @@ int main(){
 
     std::cout << greeting  <<std::endl;
 
-    std::cin>>greeting;
-    std::cout<<greeting<<std::endl;
+    std::cin  >> greeting; // only grab first character
+    std::cout << greeting<<std::endl;
 }
