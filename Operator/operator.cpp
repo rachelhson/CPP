93695b4e0f8e118117d0f804ffe0eeeed682c4da
Rachel Hyo Son <52179017+rachelhson@users.noreply.github.com>
2021-08-13 10:39:51 -0400

    Add files via upload

---
+++ Operator/operator.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+int main(){
+    int x = 5 ; //assign
+    x = 5 + 5;
+    x = 5 * 5;
+    x = 5 / 5;
+
+    double y = 10. / 4; //'dot' to maintain double division
+
+    std::cout << y << std::endl;
+
+    double z = 10 % 4;
+    std::cout << z << std::endl;
+
+    double k = 10 + (4.0/3);
+    std::cout << k << std::endl;
+}
